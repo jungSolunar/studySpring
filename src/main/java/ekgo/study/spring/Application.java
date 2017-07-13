1e4fa186e8bebb284a04a44ff1e1b9881fe58b2c
Dahae.jung <dahae.jung@atto-research.com>
2017-07-13 15:01:37 +0900

    created: embedded Tomcat

---
+++ src/main/java/ekgo/study/spring/Application.java
@@ -0,0 +1,23 @@
+package ekgo.study.spring;
+
+import ekgo.study.spring.tomcat.TomcatServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by dhjung on 2017. 7. 13..
+ */
+public class Application {
+    private static Logger logger = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String args[]) throws Exception{
+        try{
+            logger.info("Server Starting..");
+            TomcatServer tomcat = new TomcatServer();
+            tomcat.start();
+        }catch (Exception e){
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+}
